cb79302ee1286ec3ab332acba4fcce7bb21452d7
Alexandra <32397047+Littlesweetycupoftea@users.noreply.github.com>
2018-04-13 23:18:07 +0300

    Create task1.c

---
+++ task1.c
@@ -0,0 +1,10 @@
+void push(struct node* head, int data) {
+struct node* newNode =(struct node*) malloc(sizeof(struct node)); //в первоначальной версии отсутствует выделенная область памяти 
+newNode->data = data;
+newNode->next = head;
+head = newNode;
+}
+void push_test() {
+List head = build();
+push(head, 1); 
+}
