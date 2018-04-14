fa67fa23e87b4812964e09158819c54cc4738133
Alexandra <32397047+Littlesweetycupoftea@users.noreply.github.com>
2018-04-14 08:11:40 +0300

    Update task2.c

--- task2.c
+++ task2.c
@@ -1,21 +1,35 @@
-struct node* CopyList(struct node* head) 
-{
-   struct node* current = head;
-   struct node* newList = NULL;
-   struct node* tail = NULL;
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;           
+    struct node * next; 
+} node_t;
+
+typedef struct list {
+    node_t* head;      
+    unsigned int size;
+} list_t;
+
+
+int main(void)
+{     
+   node_t* current = head;
+   node_t* newList = NULL;
+   node_t* tail = NULL;
    
    while (current != NULL) 
    {
       if (newList == NULL)
       { 
-         newList = malloc(sizeof(struct node));
+         newList = malloc(sizeof(node_t));
          newList->data = current->data;
          newList->next = NULL;
          tail = newList;
       }
       else 
       {
-         tail->next = malloc(sizeof(struct node));
+         tail->next = malloc(sizeof(node_t));
          tail = tail->next;
          tail->data = current->data;
          tail->next = NULL;
