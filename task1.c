72b35f12fe4b4ff2b5dbb382bf89f8282c6e5a02
Alexandra <32397047+Littlesweetycupoftea@users.noreply.github.com>
2018-04-20 14:25:57 +0300

    Update task1.c

--- task1.c
+++ task1.c
@@ -1,9 +1,15 @@
- void push(struct node* head, int data) {
-struct node* newNode =(struct node*) malloc(sizeof(struct node)); //в первоначальной версии отсутствует выделенная область памяти 
-newNode->data = data;
-newNode->next = NULL;
-head = newNode;
-}
-void push_test(newNode) {
-List head = build();
-push(head, 1); 
+
+void push(struct node* head, int data) {
+  struct node* newNode =(struct node*) malloc(sizeof(struct node)); //в первоначальной версии отсутствует выделенная область памяти 
+  newNode->data = data;
+  newNode->next = NULL;   //
+  head = newNode;
+                                       }
+
+void push_test() {
+  newNode* /*замена List*/ head = build();    
+  push(&head /*добавка &*/, 1); 
+                                }          
+
+ 
+ 
